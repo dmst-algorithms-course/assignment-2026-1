917908286f57c5577a3cafb7314f6666a6a61e94
Pakkos03 <t8220219@aueb.gr>
2026-04-14 17:41:50 +0300

    Update perplexity.py

--- perplexity.py
+++ perplexity.py
@@ -4,4 +4,18 @@ import math
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--stride", type=int, default=512)
+    parser.add_argument("--n-ctx", dest="n_ctx", type=int, default=2048)
+    parser.add_argument(
+        "--begin-context-tokens",
+        dest="begin_context_tokens",
+        type=int,
+        default=512
+    )
+
+    parser.add_argument("input_file")
+    parser.add_argument("out_file")
   
